02a0908199a7488b8cbfaa1947b22836dbff7f0d
MayRww <mayara.wojceikovski@gmail.com>
2025-04-30 19:15:49 -0300

    model dragao criada

---
+++ DracarysAPI/Models/Dragao.cs
@@ -0,0 +1,10 @@
+namespace DracarysAPI.Models;
+
+public class Dragao
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Montador { get; set; }
+    public int CasaId { get; set; }
+
+}
