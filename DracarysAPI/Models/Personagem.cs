dcdb157ff18affc842258e46ead773389580df17
laurareded <lauramelo2308@hotmail.com>
2025-05-05 10:58:14 -0300

    model personagem e db adicionados

--- DracarysAPI/Models/Personagem.cs
+++ DracarysAPI/Models/Personagem.cs
@@ -1,14 +1,12 @@
 using System.Text.Json.Serialization;
 namespace DracarysAPI.Models;
 
-public class Dragao
+public class Personagem
 {
     public int Id { get; set; }
     public string Nome { get; set; }
-    public string Montador { get; set; }
     public int CasaId { get; set; }
 
     [JsonIgnore]
     public Casa Casa { get; set; }
-
 }
--- DracarysAPI/Program.cs
+++ DracarysAPI/Program.cs
@@ -1,15 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using DracarysAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +33,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
